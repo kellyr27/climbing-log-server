5c1fe38d20f4057ed6381befaa9b8077098e610c
Kelly <kellyriz27@gmail.com>
2024-10-22 20:22:33 +1100

    Able to test with command line arguments to turn on and off console logs

--- tests/setup.js
+++ tests/setup.js
@@ -1,18 +1,22 @@
-// Save original console methods
+// Store original console methods
 const originalConsoleLog = console.log;
 const originalConsoleError = console.error;
 const originalConsoleWarn = console.warn;
 
-// Override console methods to suppress output
+// Override console methods to suppress output if DISABLE_CONSOLE_LOG is set
 before(() => {
-  console.log = () => {};
-  console.error = () => {};
-  console.warn = () => {};
+  if (process.env.DISABLE_CONSOLE_LOG) {
+    console.log = () => {};
+    console.error = () => {};
+    console.warn = () => {};
+  }
 });
 
 // Restore original console methods after tests
 after(() => {
-  console.log = originalConsoleLog;
-  console.error = originalConsoleError;
-  console.warn = originalConsoleWarn;
+  if (process.env.DISABLE_CONSOLE_LOG) {
+    console.log = originalConsoleLog;
+    console.error = originalConsoleError;
+    console.warn = originalConsoleWarn;
+  }
 });
