42c3cc279a37a5d6fcb797ca8c66619f8a599ba4
Kelly <kellyriz27@gmail.com>
2024-10-25 16:54:52 +1100

    Improved constants

--- configs/constants.js
+++ configs/constants.js
@@ -1,6 +1,18 @@
 export const ROUTE_COLORS = ['black', 'white', 'blue', 'red', 'gray', 'green', 'yellow', 'purple', 'orange', 'pink']
 
 // NOTE: Ordered from highest to lowest status
-export const ASCENT_TICK_TYPES = ['flash', 'redpoint', 'attempt']
+const ASCENT_TICK_TYPES = [
+  { name: 'flash',
+    send: true,
+  },
+  { name: 'redpoint',
+    send: true,
+  },
+  { name: 'attempt',
+    send: false,
+  },
+]
+export const ALL_ASCENT_TICK_TYPES = ASCENT_TICK_TYPES.map(tick => tick.name)
+export const SENT_ASCENT_TICK_TYPES = ASCENT_TICK_TYPES.filter(tick => tick.send).map(tick => tick.name)
 
 export const STEEPNESS_OPTIONS = ['slab', 'vertical', 'overhung', 'roof']
